1c8d3625140189d27aa220e1ccd719c1da20905c
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-11-20 13:40:30 +0530

    Gyandeep | Added actions for the projects

---
+++ src/actions/adminAction.js
@@ -0,0 +1,4 @@
+export const SetAdmin = (data) => ({
+    type: 'SET_ADMIN',
+    payload: data,
+});
---
+++ src/actions/labelAction.js
@@ -0,0 +1,54 @@
+import { get_all_labels } from "../services/api";
+
+export const LabelsRequest = () => ({
+    type: 'FETCH_LABELS_REQUEST',
+});
+
+export const LabelsSuccess = (data) => ({
+    type: 'FETCH_LABELS_SUCCESS',
+    payload: data,
+});
+
+export const LabelsFailure = (data) => ({
+    type: 'FETCH_LABELS_FAILURE',
+    payload: data,
+});
+
+export const SetLabelsDetails = (data) => ({
+    type: 'SET_LABELS_DETAILS',
+    payload: data,
+});
+
+
+export const fetchLablesData = () => {
+    console.log('Hyyyyyy');
+    return async (dispatch) => {
+        try {
+            let data_retries = 1;
+            dispatch(LabelsSuccess([]));
+            console.log('vbhvh')
+            while (data_retries < 4) {
+                dispatch(LabelsRequest());
+                await get_all_labels()
+                    .then((response) => {
+                        console.log('bhdjbsdjb')
+                        console.log(response.data.labelsData);
+
+                        const lablesData = response.data.labelsData;
+                        if (lablesData === null) {
+                            dispatch(LabelsSuccess([]));
+                        } else {
+                            dispatch(LabelsSuccess(lablesData));
+                        }
+                        data_retries = 4;
+                    })
+                    .catch((err) => {
+                        data_retries++;
+                    });
+            }
+        } catch (err) {
+            dispatch(LabelsFailure(err));
+        }
+    };
+};
+
